28f334a6acee8c4098af5d355e289d07ba260ba4
747302196@163.com <tt19890317>
2018-12-13 16:24:18 +0800

    菜单有效

--- ZZEarth-Example/TestApp.cs
+++ ZZEarth-Example/TestApp.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.XtraBars.Ribbon;
 using Zf3DPlatform.osgEarthGUI;
+using ZfPlatform.GUI;
 using ZFGK.AddinServices;
+using ZFGK.WinForms.DvExprss.Utility;
 
 namespace ZZEarth_Example
 {
@@ -13,9 +16,16 @@ namespace ZZEarth_Example
     {
         public bool StartUp()
         {
-            var mainForm = OsgEarthForm.GetInstance();
-            mainForm.RibbonControl.Page("第一个APP").Group("测试").ItemLinks
-                .NewButton("第一个命令", btn => btn.SetTag(typeof(TestCmd)));
+            var mainRibbonForm = MainRibbonForm.GetInstance();
+            mainRibbonForm.Load += delegate(object sender, EventArgs args)
+            {
+                var osgEarthForm = mainRibbonForm.MdiChildren.Where(d => d is OsgEarthForm).Select(d => d as OsgEarthForm).First();
+                osgEarthForm.ribbonControl.Page("第一个APP").Group("测试").ItemLinks.NewButton("第一个命令", btn => btn.SetTag(typeof(TestCmd)));
+
+                // 必须重新合并Ribbbon才能有效
+                mainRibbonForm.ribbonControl.UnMergeRibbon();
+                mainRibbonForm.ribbonControl.MergeRibbon(osgEarthForm.ribbonControl);
+            };
             return true;
         }
     }
