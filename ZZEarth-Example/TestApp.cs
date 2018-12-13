ec50d2413bc3731921f403eebf29bd8e94c8a2a6
747302196@163.com <tt19890317>
2018-12-13 15:03:25 +0800

    初次提交

---
+++ ZZEarth-Example/TestApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Zf3DPlatform.osgEarthGUI;
+using ZFGK.AddinServices;
+
+namespace ZZEarth_Example
+{
+    public class TestApp : IExtendApp
+    {
+        public bool StartUp()
+        {
+            var mainForm = OsgEarthForm.GetInstance();
+            mainForm.RibbonControl.Page("第一个APP").Group("测试").ItemLinks
+                .NewButton("第一个命令", btn => btn.SetTag(typeof(TestCmd)));
+            return true;
+        }
+    }
+}
---
+++ ZZEarth-Example/TestCmd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZFGK.AddinServices;
+
+namespace ZZEarth_Example
+{
+    class TestCmd : IExtendCommand
+    {
+        public CmdResult Execute(ExtendCommandData commandData, ref string message)
+        {
+            MessageBox.Show("Hello World!");
+            return CmdResult.Succeed;
+        }
+    }
+}
