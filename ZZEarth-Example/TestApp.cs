7fe55c01aad74432df663a0bd3597bd52a8ed4f0
747302196@163.com <tt19890317>
2019-09-06 22:43:07 +0800

    适配最新的API

--- ZZEarth-Example/TestApp.cs
+++ ZZEarth-Example/TestApp.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Reflection;
 using ZFGK.AddinServices;
 using ZFGK.DvExprss.Utility;
 using ZfPlatform.DevExpressGUI;
+using ZZEarth_Example.Cmds;
 
 namespace ZZEarth_Example
 {
@@ -10,9 +12,10 @@ namespace ZZEarth_Example
         public bool StartUp()
         {
             var mainRibbonForm = MainRibbonForm.Instance;
-            mainRibbonForm.RegisterCmd<TestCmd>("test");
+            mainRibbonForm.RegisterCmd(Assembly.GetExecutingAssembly());
+
             mainRibbonForm.ribbonControl.Page("第一个APP").Group("测试")
-                .ItemLinks.NewButton("第一个命令", btn => btn.SetTag("test"));
+                .ItemLinks.NewButton("第一个命令", btn => btn.SetTag("first"));
             return true;
         }
     }
